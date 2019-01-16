0f518e4b8e1d5a1e7f31b39bcbaf6f7040ffdb47
Noah Gribbin <noahgribbin@Noahs-MacBook-Pro.local>
2019-01-16 11:51:35 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+	}
+
+	componentWillMount() {
+
+	}
+	render(){
+		return(
+			<React.Fragment>
+				<p>It is up</p>
+			</React.Fragment>
+		)
+	}
+}
+export default App
