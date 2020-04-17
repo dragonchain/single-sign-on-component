f70e47bbbda4c1ea3afc3821560f574e604f9330
Eric Tesenair <et@dragonchain.com>
2020-04-16 23:00:01 -0400

    - Move token login to Single Sign On component   - Remove token login from PrivateRoute

--- src/components/SingleSignOn/SingleSignOn.js
+++ src/components/SingleSignOn/SingleSignOn.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { cognitoApi } from '../../lib';
+import { cognitoApi, parse } from '../../lib';
 import { SSOProvider } from '../../Context';
 
 function SingleSignOn({ history, login, source, group, callback, redirect, children }) {
@@ -8,6 +8,14 @@ function SingleSignOn({ history, login, source, group, callback, redirect, child
 
   useEffect(() => {
     (async () => {
+      if (history && ['academy', 'den', 'eternal'].includes(source)) {
+        const { refreshToken } = parse(history.location.search);
+        if (refreshToken) {
+          await cognitoApi.loginWithToken(refreshToken);
+          history.push(history.location.pathname);
+        }
+      }
+
       const userData = await cognitoApi.checkSession(false);
       let isAuthenticated = !!userData;
 
@@ -15,6 +23,7 @@ function SingleSignOn({ history, login, source, group, callback, redirect, child
         const groups = await cognitoApi.groups();
         isAuthenticated = groups.includes(group);
       }
+
       const redirectTo =
         redirect || (typeof window !== 'undefined' && encodeURIComponent(window.location.href));
 
